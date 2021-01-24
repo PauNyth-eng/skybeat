6414a83097c9e4b14a2c0b7538963b1c5852b6fe
Pavel <pajko.janu@gmail.com>
2021-01-24 12:02:20 +0100

    funguje to?

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace skybeat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
