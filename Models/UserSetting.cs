c26eb9ec09515167487637712afe9f468d0e97bd
Pavel <pajko.janu@gmail.com>
2021-01-26 11:11:00 +0100

    Snad to pujde

---
+++ Models/UserSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace skybeat.Models
+{
+    public class UserSetting
+    {
+        public int id_user { get; set; }
+        public int username { get; set; }
+        public int password { get; set; }
+        public int email { get; set; }
+        public int add_date { get; set; }
+        public int id_role { get; set; }
+
+    }
+}
